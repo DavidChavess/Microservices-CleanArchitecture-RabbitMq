0862db1fcf3144ccf08ec18b29c8a0641ffda107
DavidChavess <davi.ch.fe@gmail.com>
2023-03-23 12:18:00 -0300

    add notification-api

---
+++ notification-api/src/main/kotlin/com/davidchaves/notificationapi/NotificationApiApplication.kt
@@ -0,0 +1,11 @@
+package com.davidchaves.notificationapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NotificationApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<NotificationApiApplication>(*args)
+}
---
+++ notification-api/src/test/kotlin/com/davidchaves/notificationapi/NotificationApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.davidchaves.notificationapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NotificationApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
